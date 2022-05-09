f2883ea2c635736cce0c6a0d0cad546f37466258
Julien Prugne <julien@webeleon.dev>
2022-05-09 07:41:21 +0200

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as pkg from '../package.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Pokemon battle api')
+    .setDescription(pkg.description)
+    .setVersion(pkg.version)
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('swagger', app, swaggerDocument);
+
   await app.listen(3000);
 }
 bootstrap();
